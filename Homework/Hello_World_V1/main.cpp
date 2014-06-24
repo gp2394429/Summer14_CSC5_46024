0c12428f0734ecb312a3efbc6fe120003c225816
gp2394429 <gprice1@student.rcc.edu>
2014-06-24 00:38:39 -0700

    Signed-off-by: gp2394429 <gprice1@student.rcc.edu>

---
+++ Homework/Hello_World_V1/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Guthrie Price
+ * Created on June 24, 2014, 12:35 AM
+ * Purpose: First Program Example
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    //Print "Hello World" to the standard output
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
